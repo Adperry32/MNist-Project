66473047483bb24f619b98ff2b859bc2b04118e6
Arrington Perry <perryceo21@hotmail.com>
2024-08-31 21:41:04 -0400

    Got description, shape and count of data before moving forward.

--- MNIST Training.py
+++ MNIST Training.py
@@ -6,3 +6,6 @@ filepath = "C:\\Users\\perry\\Downloads\\MNIST_train.csv"
 mnist_df = pd.read_csv(filepath)
 
 ##check size, shape, and description of data
+print(mnist_df.shape)
+print(mnist_df.count())
+print(mnist_df.describe())
